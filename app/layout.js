3fe8e37055407c7e519d01bf5ab86c26d6d1105a
nadirali1350 <nadirali1350@gmail.com>
2023-09-08 19:58:43 +0500

    add cdn tailwind

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,9 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+            <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body>
       <div className="bg-gray-950 flex flex-col justify-center items-center">
         
