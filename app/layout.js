03c3429ae61fa754daf3dcccecd09d6e52115b6e
nadirali1350 <nadirali1350@gmail.com>
2023-09-08 20:27:19 +0500

    some more

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
             <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body>
-      <div className="bg-gray-950 flex flex-col justify-center items-center">
+      <div className="bg-gray-950 flex flex-col justify-center items-center max-w-[100vw] overflow-hidden">
         
           {children}
         
